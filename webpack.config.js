2b7ce4aa03352e92819ab68babd5a78770e72489
vladdominator <https://github.com/vladdominator>
2021-06-27 18:13:21 +0300

    fix: change publicPath in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,7 @@ module.exports = ({ development }) => ({
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
     assetModuleFilename: "assets/[hash][ext]",
+    publicPath: "/",
   },
   module: {
     rules: [
